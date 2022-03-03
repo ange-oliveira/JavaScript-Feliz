3c98387e3091cce9310eb4fdfdecc52cc4b5d043
Angelica oliveira <angelica.oliveira.kaipper@gmail.com>
2022-03-03 15:52:11 -0300

    Happy javascript course start

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const root = document.querySelector("#root")
+
+function newElement(tag, content) {
+    const title = `<${tag}>${content}</${tag}>`
+
+    root.insertAdjacentHTML("beforeend", title)
+
+
+}
+
+
+newElement("h3", "eita")
+newElement("h1", "Ange")
+newElement("p", "matheus gostoso")
